82b255a37d605b059768c4714c746ae59313e087
rscritch <scritchlowpg@gmail.com>
2026-07-24 14:49:15 -0500

    Delete CommercialCore_GitHub_Deployment directory

--- CommercialCore_GitHub_Deployment/backup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if not exist backups mkdir backups
-powershell -NoProfile -Command "$ts=Get-Date -Format yyyyMMdd_HHmmss; Copy-Item 'data\commercialcore.db' ('backups\commercialcore_'+$ts+'.db')"
-echo Backup created in the backups folder.
-pause
--- CommercialCore_GitHub_Deployment/load_test_clients.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if not exist ".venv\Scripts\python.exe" call run.bat
-call .venv\Scripts\activate
-python seed_test_clients.py
-echo.
-echo Test clients loaded.
-pause
